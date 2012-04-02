7facc850d35951521be26a2b328242fda510a302
Ben Vanik <ben.vanik@gmail.com>
2012-04-01 23:15:08 -0700

    Adding Python coverage utility to start checking code coverage

--- tools/setup.sh
+++ tools/setup.sh
@@ -101,7 +101,7 @@ echo ""
 # ==============================================================================
 echo "Installing Python packages..."
 
-PYTHON_PACKAGES=( glob2 mako pil watchdog Autobahn sphinx unittest2 networkx )
+PYTHON_PACKAGES=( glob2 mako pil watchdog Autobahn sphinx unittest2 networkx coverage )
 
 for p in ${PYTHON_PACKAGES[@]}
 do
